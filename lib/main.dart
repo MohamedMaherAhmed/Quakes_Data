4a53cf674ce9895cdaa66ca8a4a53d7cdaea132c
TINYMAHER <Mohamed.dev.Maher@gmail.com>
2021-05-16 19:08:37 +0200

    third flutter app

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+main() async {
+  Map _quakeData = await getQuakes();
+
+  List _features = _quakeData['features'];
+
+  //print("${_quakeData['features'][0]['properties']['time']}");
+
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Quakes"),
+        backgroundColor: Colors.blueGrey[400],
+      ),
+      body: Center(
+        child: ListView.builder(
+            itemCount: _features.length,
+            padding: const EdgeInsets.all(15),
+            itemBuilder: (BuildContext context, int position) {
+              var _time = _features[position]['properties']['time'];
+              var format = DateFormat.yMMMMd("en_US").add_jm();
+              var date = format.format(
+                  DateTime.fromMillisecondsSinceEpoch(_time, isUtc: true));
+              return Column(
+                children: <Widget>[
+                  if (position.isEven && position != 0 || position.isOdd)
+                    new Divider(height: 5.5),
+                  ListTile(
+                    title: Text(
+                      "$date",
+                      style: TextStyle(
+                        fontSize: 15.5,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.orange,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "${_features[position]['properties']['place']}",
+                      style: TextStyle(
+                          fontSize: 13.5,
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic),
+                    ),
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.green,
+                      child: Text(
+                        "${_features[position]['properties']['mag'].toStringAsFixed(1)}",
+                        style: TextStyle(
+                          fontSize: 15.5,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ),
+                    onTap: () =>
+                        showMessageDialog(context,
+                            "${_features[position]['properties']['title']}"),
+                  ),
+                ],
+              );
+            }),
+      ),
+    ),
+  ));
+}
+
+showMessageDialog(BuildContext context, String message) {
+  var alert = AlertDialog(
+    title: Text("Quake"),
+    content: Text(message),
+    actions: <Widget>[
+      TextButton(
+        style: TextButton.styleFrom(primary: Colors.blueGrey),
+        child: Text(
+          "My Sympathies",
+          style: TextStyle(
+              fontSize: 12.9,
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.italic,
+              color: Colors.teal),
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("Thanks , My Friend"), backgroundColor: Colors.blueGrey,
+              duration: const Duration(milliseconds: 3500)));
+        },
+      ),
+    ],
+  );
+
+  showDialog(context: context, builder: (context) => alert);
+}
+
+Future<Map> getQuakes() async {
+  var apiUrl = Uri.parse(
+      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson");
+
+  http.Response response = await http.get(apiUrl);
+
+  return json.decode(response.body);
+}
